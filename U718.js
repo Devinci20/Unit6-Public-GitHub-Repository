655ad5fe94533640932568e74625ac6cb56dd139
Devinci20 <baldwin_devin_elijah@student.smc.edu>
2016-08-02 23:30:09 -0700

    Delete U718.js

--- U718.js
+++
@@ -1,9 +0,0 @@
-var shout = function () {
-    var message = "HEY YOU";
-    alert(message);
-};
-shout();
-alert(message);
-
-/** In this script, the first message is ran and alerts as "HEY YOU".
- Whereas the alert message after the shout alerts as "ReferenceError: message is not defined".
