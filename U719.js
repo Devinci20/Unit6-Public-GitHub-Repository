5bf469ed3972d0dccd7df2fd76abbafe3c7fe338
Devinci20 <baldwin_devin_elijah@student.smc.edu>
2016-08-02 23:30:35 -0700

    Delete U719.js

--- U719.js
+++
@@ -1,6 +0,0 @@
-alert("Hello");
-var alert = 2;
-alert("World");
-
-/** In this script, the first alert has no variable and since the second alert does it doesn't do anything.
- Now if I were to get rid of the variable then the script would alert, "Hello" in the first alert then "World" in the second.**/
